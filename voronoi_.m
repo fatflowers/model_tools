847630bfd716f7692b80e28ed294d8a35099beaa
sunlei <sunleihit@qq.com>
2014-12-16 20:42:42 +0800

    almost done voronoi

--- voronoi_.m
+++ voronoi_.m
@@ -9,7 +9,7 @@ I_width = 700;
 I_height = 700;
 R_vertices = [VarName1, VarName2];
 [V, C] = voronoin(R_vertices);
-
+V(1,:)=[I_width, I_height];
 voronoi_map = zeros(I_width, I_height);
 
 for i_C = 1 : length(C)
@@ -20,19 +20,23 @@ for i_C = 1 : length(C)
    end
    
    % 求i_C块泰森多边形的外界矩形
-   min_x = min(int32(xv_yv(:, 2)));
+%    min_x = min(int32(xv_yv(:, 1))) - 1;
+   min_x = min(int32(xv_yv(:, 1)));
    if min_x < 0
        min_x = 1;
    end
-   max_x = max(int32(xv_yv(:, 2)));
+%    max_x = max(int32(xv_yv(:, 1))) + 1;
+   max_x = max(int32(xv_yv(:, 1)));
    if max_x > I_height
        max_x = I_height;
    end
-   min_y = min(int32(xv_yv(:, 1)));
+   min_y = min(int32(xv_yv(:, 2)));
+%    min_y = min(int32(xv_yv(:, 2))) - 1;
    if min_y < 0
        min_y = 1;
    end
-   max_y = max(int32(xv_yv(:, 1 )));
+   max_y = max(int32(xv_yv(:, 2)));
+%    max_y = max(int32(xv_yv(:, 2))) + 1;
    if max_y > I_width
        max_y = I_width;
    end
@@ -46,20 +50,20 @@ for i_C = 1 : length(C)
        end
    end
    
-   [IN, ON] = inpolygon(x_y(:, 1), x_y(:, 2), xv_yv(:, 2), xv_yv(:, 1));
-   
+%   [IN, ON] = inpolygon(x_y(:, 1), x_y(:, 2), xv_yv(:, 1), xv_yv(:, 2));
+   IN = myinpolygon(x_y(:, 1), x_y(:, 2), xv_yv(:, 1), xv_yv(:, 2));  
    for i = 1 : length(IN)
        if IN(i) == 1
            if voronoi_map(x_y(i, 1), x_y(i, 2)) ~= 0
                sprintf('got an error!!\n');
-               break;
+               continue;
            end
            voronoi_map(x_y(i, 1), x_y(i, 2)) = i_C;
        end
        
-       if ON(i) == 1 && voronoi_map(x_y(i, 1), x_y(i, 2)) == 0
-           voronoi_map(x_y(i, 1), x_y(i, 2)) = i_C;
-       end
+%        if ON(i) == 1 && voronoi_map(x_y(i, 1), x_y(i, 2)) == 0
+%            voronoi_map(x_y(i, 1), x_y(i, 2)) = i_C;
+%        end
    end   
 end
 
