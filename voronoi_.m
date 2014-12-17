5149af3ce9985d6354d680a1ed0096d34647ce97
simon <355890577@qq.com>
2014-12-17 22:36:06 +0800

    voronoi_ get rid of Inf

--- voronoi_.m
+++ voronoi_.m
@@ -13,58 +13,60 @@ V(1,:)=[I_width, I_height];
 voronoi_map = zeros(I_width, I_height);
 
 for i_C = 1 : length(C)
-   xv_yv = zeros(length(C{i_C}), 2);
-   % put all the points indexed by C{i_C} into x_y
-   for i_i_C = 1 : length(C{i_C})
-       xv_yv(i_i_C, :) = V(C{i_C}(i_i_C), :);
-   end
-   
-   % 求i_C块泰森多边形的外界矩形
-%    min_x = min(int32(xv_yv(:, 1))) - 1;
-   min_x = min(int32(xv_yv(:, 1)));
-   if min_x < 0
-       min_x = 1;
-   end
-%    max_x = max(int32(xv_yv(:, 1))) + 1;
-   max_x = max(int32(xv_yv(:, 1)));
-   if max_x > I_height
-       max_x = I_height;
-   end
-   min_y = min(int32(xv_yv(:, 2)));
-%    min_y = min(int32(xv_yv(:, 2))) - 1;
-   if min_y < 0
-       min_y = 1;
-   end
-   max_y = max(int32(xv_yv(:, 2)));
-%    max_y = max(int32(xv_yv(:, 2))) + 1;
-   if max_y > I_width
-       max_y = I_width;
-   end
-   
-   x_y = zeros((max_x - min_x + 1) * (max_y - min_y + 1), 2);
-   k = 1;
-   for i_x = min_x : max_x
-       for i_y = min_y : max_y
-           x_y(k, :) = [i_x, i_y];
-           k = k + 1;
+    if all(C{i_C}) ~= 1
+       xv_yv = zeros(length(C{i_C}), 2);
+       % put all the points indexed by C{i_C} into x_y
+       for i_i_C = 1 : length(C{i_C})
+           xv_yv(i_i_C, :) = V(C{i_C}(i_i_C), :);
        end
-   end
-   
-%   [IN, ON] = inpolygon(x_y(:, 1), x_y(:, 2), xv_yv(:, 1), xv_yv(:, 2));
-   IN = myinpolygon(x_y(:, 1), x_y(:, 2), xv_yv(:, 1), xv_yv(:, 2));  
-   for i = 1 : length(IN)
-       if IN(i) == 1
-           if voronoi_map(x_y(i, 1), x_y(i, 2)) ~= 0
-               sprintf('got an error!!\n');
-               continue;
+
+       % 求i_C块泰森多边形的外界矩形
+    %    min_x = min(int32(xv_yv(:, 1))) - 1;
+       min_x = min(int32(xv_yv(:, 1)));
+       if min_x < 0
+           min_x = 1;
+       end
+    %    max_x = max(int32(xv_yv(:, 1))) + 1;
+       max_x = max(int32(xv_yv(:, 1)));
+       if max_x > I_height
+           max_x = I_height;
+       end
+       min_y = min(int32(xv_yv(:, 2)));
+    %    min_y = min(int32(xv_yv(:, 2))) - 1;
+       if min_y < 0
+           min_y = 1;
+       end
+       max_y = max(int32(xv_yv(:, 2)));
+    %    max_y = max(int32(xv_yv(:, 2))) + 1;
+       if max_y > I_width
+           max_y = I_width;
+       end
+
+       x_y = zeros((max_x - min_x + 1) * (max_y - min_y + 1), 2);
+       k = 1;
+       for i_x = min_x : max_x
+           for i_y = min_y : max_y
+               x_y(k, :) = [i_x, i_y];
+               k = k + 1;
            end
-           voronoi_map(x_y(i, 1), x_y(i, 2)) = i_C;
        end
-       
-%        if ON(i) == 1 && voronoi_map(x_y(i, 1), x_y(i, 2)) == 0
-%            voronoi_map(x_y(i, 1), x_y(i, 2)) = i_C;
-%        end
-   end   
+
+    %   [IN, ON] = inpolygon(x_y(:, 1), x_y(:, 2), xv_yv(:, 1), xv_yv(:, 2));
+       IN = myinpolygon(x_y(:, 1), x_y(:, 2), xv_yv(:, 1), xv_yv(:, 2));  
+       for i = 1 : length(IN)
+           if IN(i) == 1
+               if voronoi_map(x_y(i, 1), x_y(i, 2)) ~= 0
+                   sprintf('got an error!!\n');
+                   continue;
+               end
+               voronoi_map(x_y(i, 1), x_y(i, 2)) = i_C;
+           end
+
+    %        if ON(i) == 1 && voronoi_map(x_y(i, 1), x_y(i, 2)) == 0
+    %            voronoi_map(x_y(i, 1), x_y(i, 2)) = i_C;
+    %        end
+       end   
+    end
 end
 
 
